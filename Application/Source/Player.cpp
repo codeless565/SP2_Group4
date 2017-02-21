3a321721ff82343f40f59d5b6684a37162334d2d
Lim Pei Sheng <peisheng1@hotmail.com>
2017-02-21 18:33:48 +0800

    Added getters for class Player

--- Application/Source/Player.cpp
+++ Application/Source/Player.cpp
@@ -2,13 +2,13 @@
 
 Player::Player()
 {
-	Player({ 0, 0, 0 }, 0, 0, 0, 0, 0);
+	Player({ 0, 0, 0 }, 100, 5, 100, 100, 0);
 };
 Player::Player(Vector3 pos, int hp, int dmg, int ey, int oil, int sp)
 {
 	health = hp;
 	damage = dmg;
-	Position = pos;
+	position = pos;
 	energy = ey;
 	fuel = oil;
 	speed = sp;
@@ -16,9 +16,20 @@ Player::Player(Vector3 pos, int hp, int dmg, int ey, int oil, int sp)
 Player::~Player()
 {
 }
+
+void Player::InitPlayer(Vector3 pos, int hp, int dmg, int ey, int oil, int sp)
+{
+	health = hp;
+	damage = dmg;
+	position = pos;
+	energy = ey;
+	fuel = oil;
+	speed = sp;
+}
+
 Vector3 Player::getPos()
 {
-	return Position;
+	return position;
 }
 int Player::getHealth()
 {
@@ -39,4 +50,28 @@ int Player::getFuel()
 int Player::getSpeed()
 {
 	return speed;
+}
+void Player::setPos(Vector3 pos)
+{
+	position = pos;
+}
+void Player::setHealth(int hp)
+{
+	health = hp;
+}
+void Player::setDamage(int dmg)
+{
+	damage = dmg;
+}
+void Player::setEnergy(int ey)
+{
+	energy = ey;
+}
+void Player::setFuel(int oil)
+{
+	fuel = oil;
+}
+void Player::setSpeed(int sp)
+{
+	speed = sp;
 }
--- Application/Source/Player.h
+++ Application/Source/Player.h
@@ -6,24 +6,37 @@
 class Player
 {
 private:
-	Vector3 Position; //Position
+
+	
+public:	
+	Vector3 position; //Position
 	int health; // HP hitPoints
 	int damage; // DMG dealt
 	int energy; // 
 	int fuel;   // fuel
 	int speed;	
-	
-public:
+
 	Player();
 	Player(Vector3 pos, int hp, int dmg, int ey, int oil, int sp);
 	~Player();
-	
+
+	void InitPlayer(Vector3 pos, int hp, int dmg, int ey, int oil, int sp);
+
+	//Getter
 	Vector3 getPos();
 	int getHealth();
 	int getDamage();
 	int getEnergy();
 	int getFuel();
 	int getSpeed();
+	//Setter
+	void setPos(Vector3);
+	void setHealth(int hp);
+	void setDamage(int dmg);
+	void setEnergy(int ey);
+	void setFuel(int oil);
+	void setSpeed(int sp);
+
 };
 
 #endif
